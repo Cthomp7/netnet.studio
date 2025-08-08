71026d82debbaa54d1abb2877810d3468b797284
Cthomp7 <cthomp7@saic.edu>
2025-08-08 09:53:29 -0500

    added widget message to load popup

--- www/widgets/tutorial-maker-2/index.js
+++ www/widgets/tutorial-maker-2/index.js
@@ -5,13 +5,13 @@ class TutorialMaker2 extends Widget {
     this.key = 'tutorial-maker-2'
     this.title = 'Tutorial Maker 2'
     this._innerHTML = '<div></div>'
-    this.hidden = true
 
-    this.loadPopout()
+    this._createHTML()
     this.addMessageListener()
   }
 
-  async loadPopout () {
+  async _loadPopout () {
+    this.hidden = true
     this.popout = window.open(
       './widgets/tutorial-maker-2/popout/index.html',
       'tutorial-maker-popout',
@@ -37,6 +37,24 @@ class TutorialMaker2 extends Widget {
       },
       '*'
     )
+  }
+
+  _createHTML () {
+    this.innerHTML = `
+      <div class="tut-mkr-com">
+        <p class="tut-mkr-com-msg">
+        Welcome to the <span>Tutorial Maker</span>! Here you can make interactive tutorials using all sorts of useful tools. Click open to get started!
+        </p>
+        <br/>
+        <div class="tut-mkr-com-btns">
+          <button class="pill-btn pill-btn--secondary open-btn">open</button>
+          <button class="pill-btn pill-btn--secondary close-btn">close</button>
+        </div>
+      </div>
+    `
+
+    this.$('.open-btn').addEventListener('click', () => this._loadPopout())
+    this.$('.close-btn').addEventListener('click', () => this.close())
   }
 
   addMessageListener () {
